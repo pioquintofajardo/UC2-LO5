306c7b01f8acd690da67991718ef780d4937a165
Pioquinto Fajardo <76145226+pioquintofajardo@users.noreply.github.com>
2021-01-07 21:31:06 +0800

    Add files via upload

---
+++ UC2 LO5/changingstyles.js
@@ -0,0 +1,9 @@
+<p id="p1">Hello World!</p>
+<p id="p2">Hello World!</p>
+
+<script>
+document.getElementById("p2").style.color = "blue";
+document.getElementById("p2").style.fontFamily = "Arial";
+document.getElementById("p2").style.fontSize = "larger";
+</script>
+
